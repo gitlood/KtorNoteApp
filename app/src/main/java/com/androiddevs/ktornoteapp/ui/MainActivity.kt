addf9aba72f066935096aa7e6285a743e6005a97
gitlood <unilood@gmail.com>
2022-12-29 19:43:58 +1300

    Registering user working with database

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.androiddevs.ktornoteapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.androiddevs.ktornoteapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
@@ -1,19 +1,64 @@
 package com.androiddevs.ktornoteapp.ui.auth
 
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.EditText
+import android.widget.ProgressBar
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.androiddevs.ktornoteapp.R
+import com.androiddevs.ktornoteapp.other.Status
 import com.androiddevs.ktornoteapp.ui.BaseFragment
+import com.google.android.material.textfield.TextInputEditText
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthFragment : BaseFragment(R.layout.fragment_auth) {
+
+    private val viewModel: AuthViewModel by viewModels()
+    private lateinit var registerProgressBar: ProgressBar
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val loginButton = view.findViewById<Button>(R.id.btnLogin)
-        loginButton.setOnClickListener {
-            findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToNotesFragment())
+
+        registerProgressBar = view.findViewById(R.id.registerProgressBar)
+
+        val registerButton = view.findViewById<Button>(R.id.btnRegister)
+        val registerEmail = view.findViewById<TextInputEditText>(R.id.etRegisterEmail)
+        val registerPassword = view.findViewById<EditText>(R.id.etRegisterPassword)
+        val confirmPassword = view.findViewById<TextInputEditText>(R.id.etRegisterPasswordConfirm)
+
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
+
+        subscribeToObservers()
+
+        registerButton.setOnClickListener {
+            val email = registerEmail.text.toString()
+            val password = registerPassword.text.toString()
+            val confirmedPassword = confirmPassword.text.toString()
+            viewModel.register(email, password, confirmedPassword)
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.registerStatus.observe(viewLifecycleOwner, Observer { result ->
+            result?.let {
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        registerProgressBar.visibility = View.GONE
+                        showSnackBar(result.data ?: "Successfully Registered an Account")
+                    }
+                    Status.ERROR -> {
+                        registerProgressBar.visibility = View.GONE
+                        showSnackBar(result.message ?: "An Unknown Error Occurred")
+                    }
+                    Status.LOADING -> {
+                        registerProgressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
     }
 }
