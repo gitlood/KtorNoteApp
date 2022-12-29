e1dd4362102f32a72cd56057fd9b08e7c34220cf
gitlood <unilood@gmail.com>
2022-12-29 19:18:59 +1300

    Repository Update

--- app/src/main/java/com/androiddevs/ktornoteapp/repositories/NoteRepository.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/repositories/NoteRepository.kt
@@ -1,5 +1,30 @@
 package com.androiddevs.ktornoteapp.repositories
 
-class NoteRepository {
+import android.app.Application
+import com.androiddevs.ktornoteapp.data.local.NoteDao
+import com.androiddevs.ktornoteapp.data.remote.NoteApi
+import com.androiddevs.ktornoteapp.data.remote.requests.AccountRequest
+import com.androiddevs.ktornoteapp.other.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
+class NoteRepository @Inject constructor(
+    private val noteDao: NoteDao,
+    private val noteApi: NoteApi,
+    private val context: Application
+) {
+    suspend fun register(email: String, password: String) = withContext(Dispatchers.IO) {
+        try {
+            val response = noteApi.register(AccountRequest(email, password))
+            if (response.isSuccessful) {
+                Resource.success(response.body()?.message)
+            } else {
+                Resource.error(response.message(), null)
+            }
+        } catch (e: Exception) {
+            Resource.error("Couldn't connect to the servers. Check your internet connection", null)
+        }
+    }
 }
