990f693bdb698c14f7597d1fe7c824f084ebdaba
gitlood <unilood@gmail.com>
2022-12-29 15:17:54 +1300

    NavGraph added

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
@@ -1,7 +1,19 @@
 package com.androiddevs.ktornoteapp.ui.auth
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.androiddevs.ktornoteapp.R
 import com.androiddevs.ktornoteapp.ui.BaseFragment
 
 class AuthFragment : BaseFragment(R.layout.fragment_auth) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val loginButton = view.findViewById<Button>(R.id.btnLogin)
+        loginButton.setOnClickListener {
+            findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToNotesFragment())
+        }
+    }
 }
--- app/src/main/java/com/androiddevs/ktornoteapp/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/notes/NotesFragment.kt
@@ -1,7 +1,24 @@
 package com.androiddevs.ktornoteapp.ui.notes
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.androiddevs.ktornoteapp.R
 import com.androiddevs.ktornoteapp.ui.BaseFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NotesFragment : BaseFragment(R.layout.fragment_notes) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fab = view.findViewById<FloatingActionButton>(R.id.fabAddNote)
+        fab.setOnClickListener {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(
+                    ""
+                )
+            )
+        }
+    }
 }
