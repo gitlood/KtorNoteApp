dd8d631310d7e65013f035c58bd93bc55ed0e74c
gitlood <unilood@gmail.com>
2022-12-29 19:10:17 +1300

    Check for Internet Connectivity

--- app/src/main/java/com/androiddevs/ktornoteapp/other/Resource.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/other/Resource.kt
@@ -1,10 +1,6 @@
 package com.androiddevs.ktornoteapp.other
 
-data class Resource<out T>(
-    val status: Status,
-    val data: T?,
-    val message: String?
-) {
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
     companion object {
         fun <T> success(data: T?): Resource<T> {
             return Resource(Status.SUCCESS, data, null)
