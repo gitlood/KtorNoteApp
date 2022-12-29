538e144d871fec8ca886ec76c2a82e6ede7ecd88
gitlood <unilood@gmail.com>
2022-12-29 19:26:36 +1300

    View Model update

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthViewModel.kt
@@ -1,6 +1,34 @@
 package com.androiddevs.ktornoteapp.ui.auth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.ktornoteapp.other.Resource
+import com.androiddevs.ktornoteapp.repositories.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AuthViewModel: ViewModel() {
+@HiltViewModel
+class AuthViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
+
+    private val _registerStatus = MutableLiveData<Resource<String>>()
+    val registerStatus: LiveData<Resource<String>> = _registerStatus
+
+    fun register(email: String, password: String, repeatedPassword: String) {
+        _registerStatus.postValue(Resource.loading(null))
+        if (email.isEmpty() || password.isEmpty() || repeatedPassword.isEmpty()) {
+            _registerStatus.postValue(Resource.error("Please fill out all the fields", null))
+            return
+        }
+        if (password != repeatedPassword) {
+            _registerStatus.postValue(Resource.error("The passwords do not match", null))
+            return
+        }
+        viewModelScope.launch {
+            val result = repository.register(email, password)
+            _registerStatus.postValue(result)
+        }
+    }
 }
