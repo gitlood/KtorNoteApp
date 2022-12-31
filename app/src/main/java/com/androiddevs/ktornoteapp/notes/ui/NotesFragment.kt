06ee4670380081c6ac89ac24ad81df11b52da2c7
gitlood <unilood@gmail.com>
2023-01-01 10:47:03 +1300

    Class structure update

--- app/src/main/java/com/androiddevs/ktornoteapp/notes/ui/NotesFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/notes/ui/NotesFragment.kt
@@ -21,7 +21,6 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.androiddevs.ktornoteapp.R
 import com.androiddevs.ktornoteapp.core.data.local.entities.Note
-import com.androiddevs.ktornoteapp.notes.ui.adapters.NoteAdapter
 import com.androiddevs.ktornoteapp.core.util.Constants.KEY_LOGGED_IN_EMAIL
 import com.androiddevs.ktornoteapp.core.util.Constants.KEY_LOGGED_IN_PASSWORD
 import com.androiddevs.ktornoteapp.core.util.Constants.NO_EMAIL
@@ -29,6 +28,7 @@ import com.androiddevs.ktornoteapp.core.util.Constants.NO_PASSWORD
 import com.androiddevs.ktornoteapp.core.util.Event
 import com.androiddevs.ktornoteapp.core.util.Resource
 import com.androiddevs.ktornoteapp.core.util.Status
+import com.androiddevs.ktornoteapp.notes.ui.adapters.NoteAdapter
 import com.androiddevs.ktornoteapp.ui.BaseFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -50,52 +50,6 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     private val swipingItem = MutableLiveData(false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER
-        val fab = view.findViewById<FloatingActionButton>(R.id.fabAddNote)
-        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout)
-        rvNotes = view.findViewById(R.id.rvNotes)
-        setupRecyclerView()
-        subscribeToObservers()
-        setupSwipeRefreshLayout()
-
-        // The usage of an interface lets you inject your own implementation
-        val menuHost: MenuHost = requireActivity()
-
-        // Add menu items without using the Fragment Menu APIs
-        // Note how we can tie the MenuProvider to the viewLifecycleOwner
-        // and an optional Lifecycle.State (here, RESUMED) to indicate when
-        // the menu should be visible
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                // Add menu items here
-                menuInflater.inflate(R.menu.menu_notes, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                when (menuItem.itemId) {
-                    R.id.miLogout -> logout()
-                }
-                return true
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
-
-        noteAdapter.setOnItemClickListener {
-            findNavController().navigate(
-                NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
-            )
-        }
-
-        fab.setOnClickListener {
-            findNavController().navigate(
-                NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(
-                    ""
-                )
-            )
-        }
-    }
 
     private val itemTouchHelperCallBack = object : ItemTouchHelper.SimpleCallback(
         0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
@@ -137,9 +91,47 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
         }
     }
 
-    private fun setupSwipeRefreshLayout() {
-        swipeRefreshLayout.setOnRefreshListener {
-            viewModel.syncAllNotes()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fabAddNote)
+
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout)
+        rvNotes = view.findViewById(R.id.rvNotes)
+
+        setupRecyclerView()
+        subscribeToObservers()
+        setupSwipeRefreshLayout()
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.menu_notes, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.miLogout -> logout()
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        noteAdapter.setOnItemClickListener {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+            )
+        }
+
+        fab.setOnClickListener {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(
+                    ""
+                )
+            )
         }
     }
 
@@ -148,18 +140,6 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
         adapter = noteAdapter
         layoutManager = LinearLayoutManager(requireContext())
         ItemTouchHelper(itemTouchHelperCallBack).attachToRecyclerView(this)
-    }
-
-    private fun logout() {
-        sharedPref.edit().putString(KEY_LOGGED_IN_EMAIL, NO_EMAIL).apply()
-        sharedPref.edit().putString(KEY_LOGGED_IN_PASSWORD, NO_PASSWORD).apply()
-        val navOptions = NavOptions.Builder()
-            .setPopUpTo(R.id.notesFragment, true)
-            .build()
-        findNavController().navigate(
-            NotesFragmentDirections.actionNotesFragmentToAuthFragment(),
-            navOptions
-        )
     }
 
     private fun subscribeToObservers() {
@@ -184,11 +164,27 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
         }
     }
 
-    private fun onLoading(result: Resource<List<Note>>) {
-        result.data?.let { notes ->
-            noteAdapter.notes = notes
+    private fun setupSwipeRefreshLayout() {
+        swipeRefreshLayout.setOnRefreshListener {
+            viewModel.syncAllNotes()
         }
-        swipeRefreshLayout.isRefreshing = true
+    }
+
+    private fun logout() {
+        sharedPref.edit().putString(KEY_LOGGED_IN_EMAIL, NO_EMAIL).apply()
+        sharedPref.edit().putString(KEY_LOGGED_IN_PASSWORD, NO_PASSWORD).apply()
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(R.id.notesFragment, true)
+            .build()
+        findNavController().navigate(
+            NotesFragmentDirections.actionNotesFragmentToAuthFragment(),
+            navOptions
+        )
+    }
+
+    private fun onSuccess(result: Resource<List<Note>>) {
+        noteAdapter.notes = result.data!!
+        swipeRefreshLayout.isRefreshing = false
     }
 
     private fun onError(
@@ -206,8 +202,10 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
         swipeRefreshLayout.isRefreshing = false
     }
 
-    private fun onSuccess(result: Resource<List<Note>>) {
-        noteAdapter.notes = result.data!!
-        swipeRefreshLayout.isRefreshing = false
+    private fun onLoading(result: Resource<List<Note>>) {
+        result.data?.let { notes ->
+            noteAdapter.notes = notes
+        }
+        swipeRefreshLayout.isRefreshing = true
     }
 }
